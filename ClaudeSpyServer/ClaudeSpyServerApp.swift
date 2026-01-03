1838dc9ac02b7f3ce892898273b77b21603c46a0
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2026-01-03 11:23:48 -1000

    Adding macos target

---
+++ ClaudeSpyServer/ClaudeSpyServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeSpyServerApp.swift
+//  ClaudeSpyServer
+//
+//  Created by Gustavo Ambrozio on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeSpyServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeSpyServerTests/ClaudeSpyServerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeSpyServerTests.swift
+//  ClaudeSpyServerTests
+//
+//  Created by Gustavo Ambrozio on 1/3/26.
+//
+
+import Testing
+@testable import ClaudeSpyServer
+
+struct ClaudeSpyServerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
